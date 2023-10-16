a0c6e07c69565bc560d5131a68100272883dbc5a
AndreaNajeraBosonit <andrea.najera@bosonit.com>
2023-10-16 17:13:46 +0200

    tercera parte

--- block5.properties/src/main/java/com/block5/properties/block5/properties/Application.java
+++ block5.properties/src/main/java/com/block5/properties/block5/properties/Application.java
@@ -1,12 +1,14 @@
 package com.block5.properties.block5.properties;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-public class Application {
+public class Application  implements CommandLineRunner {
 	@Value("${greeting}")
 	private String greeting;
 
@@ -26,6 +28,21 @@ public class Application {
 		System.out.println("El valor de new.property es: " + newProperty);
 		String newValue = "valor de new.property";
 		System.setProperty("new.property", newValue);
+
+
 	}
+
+	/*3) Buscar la manera de leer la variable de entorno del Sistema Operativo “MYURL” mostrarla al arrancar el programa (con un CommandLineRunner).*/
+	@Override
+	public void run(String... args) throws Exception {
+		String myUrl = System.getenv("MYURL");
+		System.out.println("El valor de MYURL es: " + myUrl);
+
+		/*3)Mostrar la variable MYURL2 que por defecto tenga un valor igual a “NO_tengo_valor” pero que si existe en el S.O. muestre lo que haya en el S.O.*/
+
+		String myurl2 = System.getenv("MYURL2") != null ? System.getenv("MYURL2") : "no_tengo_valor";
+		System.out.println("El valor de MYURL2 es: " + myurl2);
+	}
+
 
 }
